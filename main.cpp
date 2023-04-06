82a57b715a39b1cbb9c02959d1bc0f95548efb0c
PandaGaming <96707062+Pandagaming15@users.noreply.github.com>
2023-04-06 10:22:51 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,6 @@ ConfigEntry* name;
 
 extern "C" void OnModLoad()
 {
-	/*
-	sautils = (ISAUtils*)GetInterface("SAUtils");
-    if(sautils != nullptr) 
-    {
-    sautils->AddSettingsTabButton("Discord", Open, "menu_maindiscord", NULL);
-    }*/
 	name = cfg->Bind("Name", "GTASAMP", "SaveFile");
 	CHook::MemCopy((char*)libs.pGame + 0x6B012C, name->GetString());
 }
